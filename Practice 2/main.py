a73d6621f01840dc3dc4155ef2b600f1eae996b9
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-09-29 23:25:38 +0300

    Prerelease version

--- Practice 2/main.py
+++ Practice 2/main.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from abc import ABC, abstractmethod
-from os import path
-from re import split, compile, match
+from os import path, getcwd
+from re import split, compile
 
 
 # region Parsers
@@ -79,9 +80,17 @@ class TextWeatherParser(WeatherParser):
     Wind direction - one of the symbols: N or NE or E or SE or S or SW or W or NW
     """
 
+    class TextLineError:
+        def __init__(self, position: int, line: list[str]):
+            self.position = position
+            self.line = line
+
+        def __str__(self):
+            return f"Line \"{' '.join(self.line)}\" at position {self.position} is invalid."
+
     def __init__(self):
         self.__date_format = compile(r"(\d{2}[/.]\d{2}[/.]\d{4})|(today)")
-        self.__time_format = compile(r"\d{2}:\d{2}")
+        self.__time_format = compile(r"\d{1,2}:\d{2}")
         self.__temperature_format = compile(r"-?\d+")
         self.__pressure_format = compile(r"\d+|(none)")
         self.__wind_format = compile(r"N|(NE)|E|(SE)|S|(SW)|W|(NW)|(none)")
@@ -101,8 +110,18 @@ class TextWeatherParser(WeatherParser):
 
     def parse_weather(self, source: str) -> dict:
         valid_lines = self.get_valid_lines(source)
-        # TODO: build a dictionary based on valid_lines
-        return {}
+
+        for line in valid_lines:
+            city, date, time, temp, press, wind = line
+            if city not in self.weather:
+                self.weather[city] = {}
+            if date not in self.weather[city]:
+                self.weather[city][date] = {}
+
+            self.weather[city][date][time] = [int(temp) if temp != 'none' else 'none',
+                                              int(press) if press != 'none' else 'none',
+                                              wind]
+        return self.weather
 
     def get_valid_lines(self, source: str) -> list[list[str]]:
         """
@@ -113,9 +132,19 @@ class TextWeatherParser(WeatherParser):
         """
 
         with open(source, 'r', encoding=self.__encoding) as file:
-            lines = [valid_line for valid_line in
-                     [[string.strip() for string in split(self.__delimiter, line) if string.strip() != ""]
-                      for line in file.readlines()] if self.is_valid_line(valid_line)]
+            lines = [[string.strip() for string in split(self.__delimiter, line) if string.strip() != ""]
+                     for line in file.readlines()]
+
+        line_errors = [self.TextLineError(position, line)
+                       for (position, line) in enumerate(lines, start=1)
+                       if not self.is_valid_line(line)]
+
+        for error in line_errors:
+            lines.remove(error.line)
+
+        to_save = [str(not_empty) for not_empty in line_errors if not_empty.line]
+        if to_save:
+            self.save('\n'.join(to_save))
 
         return lines
 
@@ -128,12 +157,141 @@ class TextWeatherParser(WeatherParser):
         """
 
         return len(line) == 6 and all([pattern.match(target) for pattern, target in zip(self.__formats, line[1:])])
+
+    def save(self, validation_errors):
+        """
+        Saves errors data to debug.
+
+        :param validation_errors: errors to debug.
+        """
+
+        file_to_save = f"{getcwd()}\\Errors" + \
+                       f"_{datetime.now().strftime('%Y_%d_%m_%H_%M_%S')}.txt"
+        with open(file_to_save, 'w+', encoding=self.__encoding) as file:
+            file.write(validation_errors)
+
+
+# endregion
+
+
+# region Operations
+
+
+class WeatherOperator:
+    HISTORY = ""
+    CITY = 0
+    DATE = 1
+    TIME = 2
+    TEMPERATURE = 3
+    PRESSURE = 4
+    WIND = 5
+    RESPONSE_SEPARATOR = '\n' + "=" * 50 + '\n'
+    CITY_NOT_FOUND = "There is no such city: {0}."
+    CITY_DATE_NOT_FOUND = "There is no info about {0} for {1}"
+    DEFAULT_FORMAT = "|{0:<12}|{1:^8}|{2:^6}|{3:^8}|{4:^4}|"
+
+    @staticmethod
+    def tracker(func):
+        def inner(*args):
+            response = f"Response for: {func.__name__}({','.join([str(arg) for arg in args[1:]])})\n" \
+                       + str(func(*args)) \
+                       + WeatherOperator.RESPONSE_SEPARATOR
+            print(response)
+            WeatherOperator.HISTORY += response
+
+        return inner
+
+    def __init__(self, weather: dict):
+        self.weather = weather
+
+    def to_list(self):
+        return [(city, date, time, self.weather[city][date][time][0],
+                 self.weather[city][date][time][1],
+                 self.weather[city][date][time][2])
+                for city in self.weather
+                for date in self.weather[city] for time in self.weather[city][date]]
+
+    @tracker
+    def format_city(self, city: str, city_format: str = None) -> str:
+        """
+        Returns weather data for the city in the set format.
+
+        :param city: city to get info about.
+        :param city_format: format to display data.
+        :returns: information about city in format - if city exists in db. Else - city not found response.
+        """
+        validated_format = city_format if city_format else self.DEFAULT_FORMAT
+        return self.CITY_NOT_FOUND.format(city) if city not in self.weather \
+            else '\n'.join([str(validated_format.format(date, time,
+                                                        self.weather[city][date][time][0],
+                                                        self.weather[city][date][time][1],
+                                                        self.weather[city][date][time][2]))
+                            for date in self.weather[city]
+                            for time in self.weather[city][date]])
+
+    @tracker
+    def max_temperature(self):
+        temperatures = self.to_list()
+        return max(temperatures, key=lambda temp: temp[self.TEMPERATURE])[self.CITY]
+
+    @tracker
+    def min_temperature(self):
+        temperatures = self.to_list()
+        return min(temperatures, key=lambda temp: temp[self.TEMPERATURE])[self.CITY]
+
+    @tracker
+    def changes(self, city: str, date: str):
+        return self.CITY_DATE_NOT_FOUND.format(city, date) \
+            if city not in self.weather or date not in self.weather[city] \
+            else ', '.join([str(item[self.TEMPERATURE]) for item in self.to_list()
+                            if item[self.CITY] == city and item[self.DATE] == date])
+
+    @tracker
+    def domain_wind(self, *cities):
+        for city in cities:
+            if city not in self.weather:
+                return self.CITY_NOT_FOUND.format(city)
+        winds = [item[self.WIND] for item in self.to_list() if item[self.CITY] in cities]
+        winds_as_dict = {wind: winds.count(wind) for wind in set(winds)}
+        return max(winds_as_dict, key=winds_as_dict.get)
+
+    @tracker
+    def filter_temp(self, predicate):
+        return '\n'.join([self.DEFAULT_FORMAT.format(item[self.CITY],
+                                                     item[self.DATE],
+                                                     item[self.TIME],
+                                                     item[self.TEMPERATURE],
+                                                     item[self.PRESSURE],
+                                                     item[self.WIND])
+                          for item in self.to_list() if item[self.TEMPERATURE] != 'none' and
+                          predicate(item[self.TEMPERATURE])])
+
+    def save_session(self):
+        if not self.HISTORY:
+            return
+        file_to_save = f"{getcwd()}\\weather_response" + \
+                       f"_{datetime.now().strftime('%Y_%d_%m_%H_%M_%S')}.txt"
+        with open(file_to_save, 'w+', encoding="utf-8") as file:
+            file.write(self.HISTORY)
+
+
 # endregion
 
 
 def main():
-    txt = TextWeatherParser()
-    txt.parse("input.txt")
+    wo = WeatherOperator(TextWeatherParser().parse_weather("input.txt"))
+    wo.format_city("Стрий", "{0}, {1}, {2}, {3}, {4}")
+    wo.format_city("Сколе")
+    wo.format_city("С")
+    wo.max_temperature()
+    wo.min_temperature()
+    wo.changes("Львів", "01.10.2022")
+    wo.changes("Львів", "29.10.2022")
+    wo.changes("Сколе", "30.09.2022")
+    wo.domain_wind("Стрий", "Львів")
+    wo.domain_wind("Сколе")
+    wo.domain_wind("Стрий", "Львів", "Золочів")
+    wo.filter_temp(lambda temp: temp > 15)
 
 
 if __name__ == "__main__":
