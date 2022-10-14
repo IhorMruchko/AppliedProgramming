2c8af46d3894249d769969bd31131a77abaf4601
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-10-14 19:01:01 +0300

    done 4 practiec

--- Practice 4/main.py
+++ Practice 4/main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from inspect import stack
 from unittest import TestCase, main
 
@@ -7,6 +8,8 @@ OPERANDS = ['x', 'y', 'z']
 OPERATORS = ['+', '-']
 OPENING_BRACKETS = ['(', '[', '{']
 CLOSING_BRACKETS = [')', ']', '}']
+NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+SEMICOLON = ','
 
 
 class StackOverflowException(BaseException):
@@ -14,6 +17,7 @@ class StackOverflowException(BaseException):
     Represents custom exception for the stack.
     Raised in case using push method and stack is full.
     """
+
     def __init__(self):
         super(StackOverflowException, self).__init__("Maximum stack size reached.")
 
@@ -23,6 +27,7 @@ class StackIsEmptyException(BaseException):
     Represents custom exception for the stack.
     Raised in case using pop method and stack is empty.
     """
+
     def __init__(self):
         super(StackIsEmptyException, self).__init__("Stack is empty.")
 
@@ -32,6 +37,17 @@ class ParenthesisMissmatchException(BaseException):
         self.open = opening
         self.close = closing
         super().__init__(f'Formula contains parenthesis missmatch: {self.open} opening to {self.close} closing.')
+
+
+class NotationException(BaseException):
+    EXCEPTION_MESSAGE = "Expected {0} but was {1} at {2}\n{3}\n{4}^"
+
+    def __init__(self, expected, present, index, line):
+        self.expected = expected
+        self.present = present
+        self.index = index
+        self.line = line
+        super().__init__(self.EXCEPTION_MESSAGE.format(expected, present, index, line, '-' * index))
 
 
 class Stack:
@@ -54,7 +70,7 @@ class Stack:
         self.__storage = []
 
     def __str__(self):
-        return f'Stack: ({self.size}/{self.capacity}) [{", ".join(self.__storage)}]'
+        return f'Stack: ({self.size}/{self.capacity}) [{", ".join(map(str, self.__storage))}]'
 
     @property
     def is_empty(self) -> bool:
@@ -144,7 +160,10 @@ class Stack:
         """
         if self.is_empty:
             raise StackIsEmptyException()
-        return self.__storage[self.__space_used-1]
+        return self.__storage[self.__space_used - 1]
+
+    def try_pop(self, default):
+        return default if self.is_empty else self.pop()
 
 
 class Case:
@@ -225,6 +244,7 @@ class TestCases:
     """
     Loads and creates list of the Cases.
     """
+
     def __init__(self, ):
         self.__cases: list[Case] = []
 
@@ -309,8 +329,303 @@ class Test(TestCase):
         expected = is_valid_formula(self.current.data)
         self.assertEqual(expected, self.current.expected)
 
+    def test_compilation(self):
+        expected = Compiler().compile(self.current.data)
+        self.assertEqual(expected, self.current.expected)
+
+
+class Compiler:
+    """
+    Compiles expression in format:
+    expression := number | S(expression, expression) | D(expression, expression)
+    number := 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0
+    """
+    def __init__(self):
+        self.state = InitialState(self)
+        self.previous_state = Stack(100)
+        self.stack = Stack(100)
+        self.expression = ""
+        self.current_index = 0
+
+    def compile(self, expression: str) -> int:
+        """
+        Evaluates expression with predefined format.
+
+        :param expression: expression to compile and evaluate
+        """
+        self.expression = expression
+        while not isinstance(self.state, FinalState):
+            self.state.accept_letter()
+        return int(sum([self.stack.try_pop(0) for _ in range(self.stack.size)]))
+
+
+class State(ABC):
+    """
+    Base of the state machine to compile the expression.
+    Provides access to the compiler data and simplifies its management.
+
+    InitialState    -> (expression was empty) FinalState
+                    -> (expression starts with the number) OneNumberState
+                    -> (expression starts with letter 'S') SumState
+                    -> (expression starts with letter 'D') DivideState
+
+    OneNumberState  -> Final state
+
+    SumState        -> (if '(' provided) FirstSumArgumentState
+
+    FirstArgument   -> (expression starts with letter 'S') SumState
+                    -> (expression starts with letter 'D') DivideState
+                    -> (',' provided) SecondArgument
+
+    SecondArgument  -> (expression starts with letter 'S') SumState
+                    -> (expression starts with letter 'D') DivideState
+                    -> (')' provided) Previous state
+
+    FinalState      -> Ends execution
+
+    """
+    EXPRESSION_END = 'end of the expression'
+    NUMBER = 'number'
+
+    def __init__(self, context: Compiler):
+        self.context = context
+
+    @property
+    def letter(self):
+        return self.context.expression[self.context.current_index] if self.context.current_index < \
+                                                                      len(self.context.expression) else ''
+
+    @property
+    def index(self):
+        return self.context.current_index
+
+    @property
+    def is_end_of_expression(self):
+        return self.index >= len(self.context.expression)
+
+    def next(self):
+        self.context.current_index += 1
+
+    def push(self, value):
+        self.context.stack.push(value)
+
+    def pop(self):
+        return self.context.stack.try_pop('0')
+
+    def change_state(self, new_state):
+        self.context.state = new_state(self.context)
+        self.next()
+
+    def change_and_save_state(self, new_state):
+        self.context.previous_state.push(self.context.state)
+        self.change_state(new_state)
+
+    def rollback_state(self):
+        if not self.context.previous_state.is_empty:
+            self.context.state = self.context.previous_state.pop()
+        self.next()
+
+    def raise_exception(self, expected: str, index=None):
+        raise NotationException(expected, self.letter,
+                                self.index if index is None else index,
+                                self.context.expression)
+
+    @abstractmethod
+    def accept_letter(self):
+        pass
+
+
+class InitialState(State):
+
+    def accept_letter(self):
+        if self.is_end_of_expression:
+            self.change_state(FinalState)
+            return
+
+        if self.letter == ' ':
+            self.next()
+            return
+
+        if self.letter in NUMBERS:
+            self.push(int(self.letter))
+            self.change_state(OneNumberState)
+            return
+
+        if self.letter == 'S':
+            self.change_state(SumState)
+            return
+
+        if self.letter == 'D':
+            self.change_state(DivideState)
+
+
+class OneNumberState(State):
+
+    def accept_letter(self):
+        if self.is_end_of_expression:
+            self.change_state(FinalState)
+            return
+
+        if self.letter == ' ':
+            self.next()
+            return
+
+        if self.letter in NUMBERS:
+            self.push(self.pop() + self.letter)
+            self.next()
+            return
+
+        self.raise_exception(self.EXPRESSION_END)
+
+
+class OperatorState(State):
+
+    def accept_letter(self):
+        if self.letter == ' ':
+            self.next()
+            return
+
+        if self.letter != '(':
+            self.raise_exception('(')
+
+
+class SumState(OperatorState):
+
+    def accept_letter(self):
+        super(SumState, self).accept_letter()
+        self.change_state(FirstSumArgumentState)
+
+
+class DivideState(OperatorState):
+    def accept_letter(self):
+        super(DivideState, self).accept_letter()
+        self.change_state(FirstDivideArgumentState)
+
+
+class ArgumentInputState(State, ABC):
+
+    def handle_argument_input(self):
+        if self.letter == ' ':
+            self.next()
+            return True
+
+        if self.letter == 'S':
+            self.change_and_save_state(SumState)
+            return True
+
+        if self.letter == 'D':
+            self.change_and_save_state(DivideState)
+            return True
+
+        return False
+
+
+class FirstSumArgumentState(ArgumentInputState):
+    def accept_letter(self):
+        if super(FirstSumArgumentState, self).handle_argument_input():
+            return
+
+        if self.letter == ',':
+            self.change_state(SecondSumArgumentInputState)
+            return
+
+        if self.is_end_of_expression or self.letter == ')':
+            self.raise_exception(',')
+
+        if self.letter not in NUMBERS:
+            self.raise_exception(self.NUMBER)
+
+        self.push(int(self.letter))
+        self.next()
+
+
+class SecondSumArgumentInputState(ArgumentInputState):
+
+    def accept_letter(self):
+        if super(SecondSumArgumentInputState, self).handle_argument_input():
+            return
+
+        if self.is_end_of_expression:
+            self.change_state(FinalState)
+            return
+
+        if self.letter == ')':
+            second, first = self.pop(), self.pop()
+            self.push(first+second)
+            self.rollback_state()
+            return
+
+        if self.letter not in NUMBERS:
+            self.raise_exception(self.NUMBER)
+
+        self.push(int(self.letter))
+        self.next()
+
+
+class FirstDivideArgumentState(ArgumentInputState):
+
+    def accept_letter(self):
+        if super(FirstDivideArgumentState, self).handle_argument_input():
+            return
+
+        if self.is_end_of_expression or self.letter == ')':
+            self.raise_exception(',')
+            
+        if self.letter == ',':
+            self.change_state(SecondDivideArgumentInputState)
+            return
+
+        if self.letter not in NUMBERS:
+            self.raise_exception(self.NUMBER)
+
+        self.push(int(self.letter))
+        self.next()
+
+
+class SecondDivideArgumentInputState(ArgumentInputState):
+
+    def accept_letter(self):
+        if super(SecondDivideArgumentInputState, self).handle_argument_input():
+            return
+
+        if self.is_end_of_expression:
+            self.change_state(FinalState)
+            return
+
+        if self.letter == ')':
+            second, first = self.pop(), self.pop()
+            if second == 0:
+                self.raise_exception('number from 1 to 9', self.index - self.context.previous_state.size * 2)
+            self.push(first / second)
+            self.rollback_state()
+            return
+
+        if self.letter not in NUMBERS:
+            self.raise_exception(self.NUMBER)
+
+        if self.letter == '0':
+            self.raise_exception('number from 1 to 9')
+
+        self.push(int(self.letter))
+        self.next()
+
+
+class FinalState(State):
+    def accept_letter(self):
+        return
+
 
 def get_parenthesis(line: str, sort_by: int = -1) -> list[tuple]:
+    """
+    Gather information about indexes of the parenthesis.
+
+    :param line: line to parse indexes of the parenthesis from.
+    :param sort_by: sorting order of the indexes:   -1 -not sorted,
+                                                    0 sorted by opening parenthesis,
+                                                    1 or grater sorted by closing parenthesis.
+    :raise ParenthesisMissmatchException: amount of the closing parenthesis is not equal to opening ones.
+    :returns: list of the tuples of the parenthesis.
+    """
     open_parenthesis_amount = line.count(OPENING_PARENTHESIS)
     close_parenthesis_amount = line.count(CLOSING_PARENTHESIS)
     if open_parenthesis_amount != close_parenthesis_amount:
@@ -346,13 +661,13 @@ def is_valid_formula(line: str) -> bool:
     :param line: line to validate as formula
     :returns: True - if line is satisfying formula requirements. False - otherwise.
     """
-    s1 = Stack(len(line))
-    s2 = Stack(len(line))
+    operands_stack = Stack(len(line))
+    operators_stack = Stack(len(line))
     is_letter_can_be_next = True
 
     for letter in line.replace(' ', ''):
         if letter in OPERANDS:
-            s1.push(letter)
+            operands_stack.push(letter)
             if is_letter_can_be_next:
                 is_letter_can_be_next = False
             else:
@@ -360,36 +675,32 @@ def is_valid_formula(line: str) -> bool:
 
         if letter in OPERATORS:
             is_letter_can_be_next = True
-            s2.push(letter)
+            operators_stack.push(letter)
 
         if letter in OPENING_BRACKETS:
-            s2.push(letter)
+            operators_stack.push(letter)
 
         if letter in CLOSING_BRACKETS:
-            while not s2.is_empty:
-                if letter == get_closing_bracket(s2.pop()):
+            while not operators_stack.is_empty:
+                if letter == get_closing_bracket(operators_stack.pop()):
                     break
-                if s1.size < 2:
+                if operands_stack.size < 2:
                     return False
-                s1.pop()
+                operands_stack.pop()
             else:
                 return False
 
-    while not s2.is_empty:
-        if s2.pop() not in OPERATORS or s1.size < 2:
+    while not operators_stack.is_empty:
+        if operators_stack.pop() not in OPERATORS or operands_stack.size < 2:
             return False
-        s1.pop()
+        operands_stack.pop()
 
-    return s1.size <= 1 and s2.is_empty
-
-
-def evaluate_formula(line: str) -> float:
-    pass
+    return operands_stack.size <= 1 and operators_stack.is_empty
 
 
 def run():
-    line = "x - (y - z - (x + x))"
-    print(is_valid_formula(line))
+    c = Compiler()
+    print(c.compile("D(S(9, S(2, 9)), D(4, 2))"))
 
 
 if __name__ == '__main__':
