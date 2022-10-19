3098278abd28c061bc0fb29ec0d3e430a0bc09ad
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-10-19 18:54:48 +0300

    Done 3 task

--- Practice 6/main.py
+++ Practice 6/main.py
@@ -1,6 +1,17 @@
 import os
 import time
 from datetime import datetime
+from enum import Enum
+
+
+class FileType(Enum):
+    Doc = ('.doc', '.docx')
+    PDF = ('.pdf', '.djv')
+    Image = ('.jpeg', '.png', '.gif')
+    Programs = ('.py', '.dll', '.exe')
+    Transfers = ('.json', '.xml')
+    Common = ('.txt', '.txt')
+    Folders = ('', '')
 
 
 class FileManager:
@@ -31,6 +42,34 @@ class FileManager:
         raise ValueError("Time selector string mus be of of the literals: "
                          "'c', 'creation', 'create', 'm', 'modification', 'mod', 'a', 'access', 'acs'. "
                          f"Instead {_time}")
+
+    @staticmethod
+    def is_type_matches(file_types: tuple[str] | str | FileType, file_type: str):
+        """
+        Checks is file type is fit finding file types.
+
+        :raises TypeError: file types must be only string-tuple, string or FileType enum.
+        :returns: True - if file type is in or equal file_types. False - otherwise.
+        """
+        if isinstance(file_types, tuple):
+            return file_type in file_types
+        if isinstance(file_types, FileType):
+            return file_type in file_types.value
+        if isinstance(file_types, str):
+            return file_type == file_types
+
+        raise TypeError(f'file_types can be only string-tuple, string or FileType enum. Instead {type(file_types)}')
+
+    @staticmethod
+    def get_file_type(file_types: tuple[str] | str | FileType) -> str:
+        if isinstance(file_types, tuple):
+            return ','.join(file_types)
+        if isinstance(file_types, FileType):
+            return ','.join(file_types.value)
+        if isinstance(file_types, str):
+            return file_types
+
+        raise TypeError(f'file_types can be only string-tuple, string or FileType enum. Instead {type(file_types)}')
 
     @staticmethod
     def order_files_by_date(dir_path: str, _time: str = "creation", reverse: bool = False) -> list[tuple]:
@@ -69,6 +108,11 @@ class FileManager:
 
         return result
 
+    @staticmethod
+    def find_by_type(file_types: tuple[str] | str | FileType, *file_path: str) -> dict:
+        return {directory: [file_name for file_name in os.listdir(directory)
+                            if FileManager.is_type_matches(file_types, os.path.splitext(file_name)[1])]
+                for directory in file_path}
 
 class TaskSolver:
     """
@@ -77,6 +121,7 @@ class TaskSolver:
     FIRST_TASK_RESPONSE_FORMAT = '{0} recent files:\n\t{1}\n{0} older files:\n\t{2}'
     FILE_TIME_FORMAT = '{0} ({1:%d/%m/%Y %H:%m:%S}).'
     SECOND_TASK_RESPONSE_FORMAT = '{0} ({1}) are in:\n\t{2}'
+    THIRD_TASK_RESPONSE_FORMAT = 'Files of type {0} in {1} are:\n\t{2}'
 
     @staticmethod
     def top_bottom_files_date(file_path: str, _time: str = 'creation', take: int = 2):
@@ -112,8 +157,22 @@ class TaskSolver:
                          for size in parsed_data[name].keys()
                          if len(parsed_data[name][size]) > 1)
 
+    @staticmethod
+    def find_by_type(file_types: tuple[str] | str | FileType, *file_path: str):
+        dictionary = FileManager.find_by_type(file_types, *file_path)
+        return '\n'.join(TaskSolver.THIRD_TASK_RESPONSE_FORMAT.format(FileManager.get_file_type(file_types),
+                                                                      directory,
+                                                                      '\n\t'.join(dictionary[directory]))
+                         for directory in dictionary
+                         if len(dictionary[directory]) > 0)
+
 
 def main():
+    print(TaskSolver.find_by_type(FileType.Image,
+                                   r'D:\Studying\4 grade\Information security\Практичні',
+                                   r'D:\Studying\4 grade\Ukrainian language',
+                                   r'C:\Users\Lenovo\Documents',
+                                   r'E:\OperaDownload'))
     print(TaskSolver.find_duplicated_file(r'E:\Programming\Python\AppliedProgramming\Practice 1',
                                           r'E:\Programming\Python\AppliedProgramming\Practice 2',
                                           r'E:\Programming\Python\AppliedProgramming\Practice 3',
